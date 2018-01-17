b42abbd17f0082699bf01fc405d82430339efad9
huangyue123 <yue.huang@huiway.com>
2018-01-17 19:21:38 +0800

    结构修改

--- src/main/java/com/cmos/web/dao/user/SysLogDao.java
+++
@@ -1,9 +0,0 @@
-package com.cmos.web.dao.syslog;
-
-import com.cmos.web.base.dao.IDao;
-
-/**
- * Created by  on 2017/12/21.
- */
-public interface SysLogDao<T>  extends IDao<T>{
-}
--- src/main/java/com/cmos/web/iservice/user/SysLogSV.java
+++
@@ -1,9 +0,0 @@
-package com.cmos.web.iservice.user;
-
-import com.cmos.web.base.iservice.IService;
-
-/**
- * Created by Administrator on 2017/12/21.
- */
-public interface IUserSV<T> extends IService<T> {
-}
